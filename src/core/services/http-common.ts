37fb5944134bde739adfb5f8d5d00fc4bd6aaf29
Rojoxt <delamarzp@gmail.com>
2024-09-25 10:47:29 -0500

    fix: fixed api

--- src/core/services/http-common.ts
+++ src/core/services/http-common.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 //API runs in port 3000, consider this for all services.js
+
 export default axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: process.env.VUE_APP_API_URL,
   //'https://barhand-api.herokuapp.com/api/v1',
   headers: { 'Content-type': 'application/json' }
 });
