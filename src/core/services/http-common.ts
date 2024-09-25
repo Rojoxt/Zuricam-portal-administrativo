87c59b965eb7bd21b3ed0787c11260cc20c6dd8f
Rojoxt <delamarzp@gmail.com>
2024-09-25 13:12:45 -0500

    fix: fixed api .ts v5

--- src/core/services/http-common.ts
+++ src/core/services/http-common.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 //API runs in port 3000, consider this for all services.js
 const apiUrl = process.env.API_URL || 'http://localhost:8080';
+console.log('result', import.meta.env.VITE_API_URL);
+
 console.log('API_URL:', apiUrl);
 console.log('API_URL2:', process.env.API_URL);
 export default axios.create({
