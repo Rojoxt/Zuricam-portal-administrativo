7cb6d7ef519c7c487c2d963b08c9adaf6242a1ad
Rojoxt <delamarzp@gmail.com>
2024-09-25 12:40:53 -0500

    fix: fixed api .ts v4

--- src/core/services/http-common.ts
+++ src/core/services/http-common.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //API runs in port 3000, consider this for all services.js
-const apiUrl = process.env.VITE_API_URL
-console.log("API Base URL:", apiUrl);
+const apiUrl = process.env.VITE_API_URL;
+console.log('API Base URL:', apiUrl);
 export default axios.create({
   baseURL: apiUrl,
   //'https://barhand-api.herokuapp.com/api/v1',
