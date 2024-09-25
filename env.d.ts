b3ece649a86c1a32c79fd08e692514499365ab92
Rojoxt <delamarzp@gmail.com>
2024-09-25 11:56:50 -0500

    fix: fixed api .ts v3

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 declare let process: {
   env: {
-    VUE_APP_API_URL: string;
+    VITE_API_URL: string;
     // Aquí podéis declarar otras variables de entorno si las usáis
   };
 };
--- src/core/services/http-common.ts
+++ src/core/services/http-common.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 //API runs in port 3000, consider this for all services.js
-
+const apiUrl = process.env.VITE_API_URL
+console.log("API Base URL:", apiUrl);
 export default axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
+  baseURL: apiUrl,
   //'https://barhand-api.herokuapp.com/api/v1',
   headers: { 'Content-type': 'application/json' }
 });
